2e873f332fb3c455a0ebedcf0ca697613818b8e9
your-SamGreenwood <your-sagg_84@hotmail.com>
2024-02-22 15:31:16 -0400

    added ToDo functions for licensing and generating markdown

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,17 +1,40 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {}
+if (license) {
+  // Implement logic to generate license badge based on the license
+  return `[![License](https://img.shields.io/badge/License-${license}-blue.svg)](LICENSE)`;
+} else {
+  return "";
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license) {
+    // Implement logic to generate license section based on the license
+    return `## License\nThis project is licensed under the ${license} license.\n`;
+  } else {
+    return "";
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license) {
+    // Implement logic to generate license section based on the license
+    return `## License\nThis project is licensed under the ${license} license.\n`;
+  } else {
+    return "";
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
+  const licenseBadge = renderLicenseBadge(data.license);
+  const licenseSection = renderLicenseSection(data.license);
+
   return `# ${data.title}
 
 `;
